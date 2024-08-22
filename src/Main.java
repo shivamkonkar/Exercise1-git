02f2cdb71e3d6834e47248fbe9c76e15347387b7
Shivam <70581178+shivamkonkar@users.noreply.github.com>
2024-08-22 13:51:52 +0530

    Add function added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class calculator{
+    public int add(int num1 , int num2){
+        return num1+num2;
+    }
+}
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

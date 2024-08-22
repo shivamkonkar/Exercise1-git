437d514cae077b226e49ca97486854ccdc0f9ba8
Shivam <70581178+shivamkonkar@users.noreply.github.com>
2024-08-22 13:56:46 +0530

    features push to remote

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ class calculator{
         return num1+num2;
     }
     public int sub(int num1 , int num2){return num1-num2;}
+    public int mul(int num1 , int num2){return num1*num2;}
     
+
 }
 
 
